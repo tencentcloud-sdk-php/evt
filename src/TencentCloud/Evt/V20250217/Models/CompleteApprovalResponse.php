af7464d92507280d1e4135e8da23c9edc1b206b0
tencentcloudapi <tencentcloudapi@tencent.com>
2025-12-17 16:26:17 +0800

    release 3.0.1504

--- src/TencentCloud/Evt/V20250217/Models/CompleteApprovalResponse.php
+++ src/TencentCloud/Evt/V20250217/Models/CompleteApprovalResponse.php
@@ -18,27 +18,19 @@ namespace TencentCloud\Evt\V20250217\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * CreateRoleUser返回参数结构体
+ * CompleteApproval返回参数结构体
  *
- * @method string getUserId() 获取<p>人员ID</p>
- * @method void setUserId(string $UserId) 设置<p>人员ID</p>
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class CreateRoleUserResponse extends AbstractModel
+class CompleteApprovalResponse extends AbstractModel
 {
-    /**
-     * @var string <p>人员ID</p>
-     */
-    public $UserId;
-
     /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
-     * @param string $UserId <p>人员ID</p>
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -54,10 +46,6 @@ class CreateRoleUserResponse extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("UserId",$param) and $param["UserId"] !== null) {
-            $this->UserId = $param["UserId"];
-        }
-
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
